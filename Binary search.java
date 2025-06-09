dfd5e5e7662ef10c438a58117e1e7679ebb99ad1
hazracodegit <dudekulahazrabee@gmail.com>
2025-06-09 10:44:44 +0530

    Create Binary search.java

---
+++ Binary search.java
@@ -0,0 +1,18 @@
+// Binary Search 
+public class BinarySearch{
+    public static void main(String[] args){
+     int[] arr= {1,3,5,7,9};
+     int target= 4;
+    int low =0,high= arr.length-1;
+     while (low<=high){
+      mid =(low+high)/2;
+        if (arr[mid]==target)
+         System.out.println("Found");
+        else if (target>arr[mid])
+          low=mid+1;
+         else
+          high=mid-1;
+        }
+   System.out.println("Not found");
+    }
+}
